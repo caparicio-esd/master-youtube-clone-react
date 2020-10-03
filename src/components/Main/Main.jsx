4b5db4f868ab866371bea4022cb7de381cf1a959
Carlos Aparicio <carlos_non86@msn.com>
2020-10-03 19:05:59 +0200

    Video Card list map

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,54 +1,13 @@
 import React from 'react';
 import VideoCard from './VideoCard';
+import videosData from './videosData.json';
 
 const Main = () => {
     return (
         <div className="main">
-            <VideoCard
-                cardContent={{
-                    title:
-                        'Robert Glasper Experiment - Everybody Loves The Sunshine (Tribute to Roy Ayers ft. Bilal) | Qwest TV',
-                    authorPic:
-                        'https://yt3.ggpht.com/a-/AOh14GiHO-rFk-WtIZK3ZM0MNBccFv-vgkkDDqJ3dw=s68-c-k-c0x00ffffff-no-rj-mo',
-                    authorUser: 'Qwest TV',
-                    authorVerified: true,
-                    visualizations: 151124,
-                    time: 'hace 3 horas',
-                    duration: '3.03',
-                    picture:
-                        'https://i.ytimg.com/vi/Mi0oAKOKQUo/hq720.jpg?sqp=-oaymwEZCNAFEJQDSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLAl9ViNHkFs7I8z6mHepb_-fUOgAw',
-                }}
-            />
-            <VideoCard
-                cardContent={{
-                    title:
-                        'Robert Glasper Experiment - Everybody Loves The Sunshine (Tribute to Roy Ayers ft. Bilal) | Qwest TV',
-                    authorPic:
-                        'https://yt3.ggpht.com/a-/AOh14GiHO-rFk-WtIZK3ZM0MNBccFv-vgkkDDqJ3dw=s68-c-k-c0x00ffffff-no-rj-mo',
-                    authorUser: 'Qwest TV',
-                    authorVerified: true,
-                    visualizations: 151124,
-                    time: 'hace 3 horas',
-                    duration: '3.03',
-                    picture:
-                        'https://i.ytimg.com/vi/Mi0oAKOKQUo/hq720.jpg?sqp=-oaymwEZCNAFEJQDSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLAl9ViNHkFs7I8z6mHepb_-fUOgAw',
-                }}
-            />
-            <VideoCard
-                cardContent={{
-                    title:
-                        'Robert Glasper Experiment - Everybody Loves The Sunshine (Tribute to Roy Ayers ft. Bilal) | Qwest TV',
-                    authorPic:
-                        'https://yt3.ggpht.com/a-/AOh14GiHO-rFk-WtIZK3ZM0MNBccFv-vgkkDDqJ3dw=s68-c-k-c0x00ffffff-no-rj-mo',
-                    authorUser: 'Qwest TV',
-                    authorVerified: true,
-                    visualizations: 151124,
-                    time: 'hace 3 horas',
-                    duration: '3.03',
-                    picture:
-                        'https://i.ytimg.com/vi/Mi0oAKOKQUo/hq720.jpg?sqp=-oaymwEZCNAFEJQDSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLAl9ViNHkFs7I8z6mHepb_-fUOgAw',
-                }}
-            />
+            {videosData.map((video, i) => (
+                <VideoCard key={i} cardContent={video} />
+            ))}
         </div>
     );
 };
